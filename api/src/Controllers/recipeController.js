8b7ade16971ad7eb440b7cd56c7ea0660421941e
Valentino Russo Barbagelata <98824327+tinorusso1012@users.noreply.github.com>
2022-07-18 21:04:05 -0300

    Update recipeController.js

--- api/src/Controllers/recipeController.js
+++ api/src/Controllers/recipeController.js
@@ -5,78 +5,18 @@ const axios = require("axios");
 require("dotenv").config();
 const { API_KEY, CANT_RECIPE } = process.env;
 
-const GetSpoonacularScore = (summary) => {
-  let index = summary.indexOf("score of") + 9;
-  let number = summary.substring(index, [index + 4]);
-  let numberFinal = "";
-  for (let i = 0; i < number.length; i++) {
-    if (isNaN(number[i]) == false) {
-      numberFinal = numberFinal + number[i];
-    }
-  }
-  return Number(numberFinal);
-};
-const GetSummaryGood = (summary) => {
-  summary = summary.replaceAll("<b>", "");
-  summary = summary.replaceAll("</b>", "");
-  summary = summary.replaceAll("<a>", "");
-  summary = summary.replaceAll("</a>", "");
-  summary = summary.replaceAll("<a", "");
-  summary = summary.replaceAll(">", "");
-  summary = summary.replaceAll("href=", "");
-
-  return summary;
-};
-// `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=${CANT_RECIPE}&addRecipeInformation=true`
-//  ApiResult.data?.results.map((e)
-const getRecipe = async () => {
-  const ApiResult = await axios.get(
-    `https://api-pi-food-default-rtdb.firebaseio.com/results.json`
-  );
-  const result = await ApiResult.data?.map((e) => {
-    return {
-      id: e.id,
-      title: e.title,
-      summary: GetSummaryGood(e.summary),
-      image: e.image,
-      preparationTime: e.preparationMinutes,
-      readyInMinutes: e.readyInMinutes,
-      diets: e.diets.map((e) => {
-        return { name: e };
-      }),
-      Instructions: e.analyzedInstructions[0]?.steps.map((e) => {
-        return e.step;
-      }),
-      dishTypes: e.dishTypes.map((e) => {
-        return { name: e };
-      }),
-      healtScore: e.healthScore,
-      servings: e.servings,
-      spoonacularScore: GetSpoonacularScore(e.summary),
-    };
-  });
-  return result;
-};
-
 const getRecipeFromDb = async () => {
-  const infoRecipe = await Recipe.findAll({
-    include: {
-      model: Diet,
-      attributes: ["name"],
-    },
-  });
+  const infoRecipe = await Recipe.findAll();
   return infoRecipe;
 };
 
 const getApiAndDb = async () => {
-  let API = await getRecipe();
   let DB = await getRecipeFromDb();
-  const GetTotal = [...API, ...DB];
+  const GetTotal = [...DB];
   return GetTotal;
 };
 
 module.exports = {
-  getRecipe,
   getRecipeFromDb,
   getApiAndDb,
 };
